a51fc3dab1c482a0ac0465f23cb2ce02c2afd710
Ethan Matthews <w0420789@nscc.ca>
2019-11-05 10:22:46 -0400

    Created Azure database.  Modified App to connect to DevDB or ProdDB.

--- VideoGameApplication/VideoGameDAL/DataBase.cs
+++ VideoGameApplication/VideoGameDAL/DataBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SqlClient;
+using System.Configuration;
 
 namespace VideoGameDAL
 {
@@ -8,26 +9,20 @@ namespace VideoGameDAL
         {
             get
             {
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
-                                            Initial Catalog=VideoGameDB;
-                                            Integrated Security=True;
-                                            Connect Timeout=60;Encrypt=False;
-                                            TrustServerCertificate=False;
-                                            ApplicationIntent=ReadWrite;
-                                            MultiSubnetFailover=False";
+                //string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VideoGameDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                string connectionString = "Data Source=videogamedb-server.database.windows.net;Initial Catalog=VideoGameDB(Azure);User ID=w0420789;Password=student1!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 return connectionString;
             }
         }
 
         public static SqlConnection GetConnection()
         {
-            SqlConnection connection = new SqlConnection(ConnectionString);
+            string activeCS = ConfigurationManager.AppSettings["activeConnectionString"];
+            string conString = ConfigurationManager.ConnectionStrings[activeCS].ConnectionString;
+            SqlConnection connection = new SqlConnection(conString);
             connection.Open();
 
             return connection;
         }
-
-
-
     }
 }
