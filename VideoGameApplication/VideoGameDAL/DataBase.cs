7831739ca89a0662a32b695cd3b53e3e7f202870
Ethan-Matthews <43070753+Ethan-Matthews@users.noreply.github.com>
2019-12-04 20:01:42 -0400

    Update DataBase.cs

--- VideoGameApplication/VideoGameDAL/DataBase.cs
+++ VideoGameApplication/VideoGameDAL/DataBase.cs
@@ -9,8 +9,8 @@ namespace VideoGameDAL
         {
             get
             {
-                //string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VideoGameDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-                string connectionString = "Data Source=videogamedb-server.database.windows.net;Initial Catalog=VideoGameDB(Azure);User ID=w0420789;Password=student1!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VideoGameDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                // string connectionString = "Data Source=videogamedb-server.database.windows.net;Initial Catalog=VideoGameDB(Azure);User ID=;Password=;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 return connectionString;
             }
         }
